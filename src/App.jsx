f8ad12e8381bc16fd0e902af8b983b206b6a20bb
Ashish Waikar <71018023+AshishW@users.noreply.github.com>
2025-04-28 15:29:46 +0000

    updates to the star component + useCallback for optimization + useEffect for updating the product rating in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,16 @@
 // import './App.css';
 import StarRating from './StarRatingComponent'
+import { useState } from 'react';
+
 function App() {
-  return <StarRating/>
+  const [productRating, setProductRating] = useState(0);
+
+  return (
+  <>
+    <h1>Product Rating: {productRating}</h1>
+    <StarRating maxRating={5} onChange={setProductRating}/>
+  </>
+  )
 }
 
 export default App;
--- src/StarRatingComponent.jsx
+++ src/StarRatingComponent.jsx
@@ -14,28 +14,33 @@ ans: assume the stars are initially unselected. if you hover over star 2, the st
 que: Can you reset the star rating? (meaning that if you have 4 stars selected, is there a way to make it to default where no stars are selected?)
 ans: Yes assume you can reset the star rating. if you click on same value which you already selected again then it will reset to default value
 */
-import { useEffect, useState } from "react"
+import { useEffect, useState, useCallback } from "react"
 import './star-rating-component.css'
 
-const StarRating = () =>{
+const StarRating = ({maxRating=5, onChange}) =>{
   const [currRating, setCurrRating] = useState(0);
   const [hoveredRating, setHoveredRating] = useState(0);
 
-  const maxRating = 5
+
+  useEffect(()=>{
+    onChange(currRating)
+  }, [currRating])
+
   
-  const handleClick = (ratingValue) =>{
+  const handleClick = useCallback((ratingValue) =>{
     if(currRating === ratingValue){
         setCurrRating(0)
     }else{
         setCurrRating(ratingValue)
     }
-  }
+  },[currRating])
+
 
 
   return (
     <div className = 'star-rating-container'>
     {
-      [...Array(5)].map((_,idx)=>{
+      [...Array(maxRating)].map((_,idx)=>{
         const ratingValue = idx + 1 
         return <p className={`star ${ratingValue <= (hoveredRating || currRating)? 'active': ''}`}
             key={idx} 
@@ -43,7 +48,8 @@ const StarRating = () =>{
             onMouseEnter={()=>setHoveredRating(ratingValue)}
             onMouseLeave={()=>setHoveredRating(0)}
             >
-                {ratingValue}
+                {/* {ratingValue} */}
+                &#9733; {/*star symbol */}
             </p>
       })
     }
